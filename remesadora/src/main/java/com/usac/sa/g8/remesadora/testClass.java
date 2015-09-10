3af52403971a2519af6d206ff792df1e66b37ebe
ReimerGhost <rchamale10@gmail.com>
2015-09-10 12:59:18 -0600

    Actualizando archivos

---
+++ remesadora/src/main/java/com/usac/sa/g8/remesadora/testClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.usac.sa.g8.remesadora;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class testClass {
+    
+}
