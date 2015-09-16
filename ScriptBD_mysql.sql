07108dbcc9eac7a083c645f6648abcefbf6fcf98
ReimerGhost <rchamale10@gmail.com>
2015-09-16 08:26:36 -0600

    SQL Mejorado

--- ScriptBD_mysql.sql
+++ ScriptBD_mysql.sql
@@ -3,7 +3,7 @@ create database bancoG8;
 use remesadoraG8;
 --TIPO DE REMESA
 create table tipo_remesa
-( id_tipo int primary key,
+(id_tipo int primary key,
 nombre varchar(20));
 --CLIENTE
 create table cliente
@@ -19,14 +19,26 @@ nombre varchar(20)
 );
 --REMESA
 create table remesa
-( id_tipo int primary key,
+( id_remesa int primary key,
 nombre_emisor varchar(20),
 apellido_receptor varchar(20),
-fecha_agregado Datetime
-id_receptor int not null REFERENCES cliente(id_cliente)
+fecha_agregado Datetime,
+id_receptor int not null,
+FOREIGN KEY (id_receptor)
+      REFERENCES cliente(id_cliente)
+      ON UPDATE CASCADE ON DELETE RESTRICT
 );
 --HISTORIAL DE REMESA
 create table historial
-( id_tipo int primary key,
-nombre varchar(20)
+(
+id_remesa int not null,
+id_estado int not null,
+fecha Datetime,
+FOREIGN KEY (id_remesa)
+      REFERENCES remesa(id_remesa)
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+FOREIGN KEY (id_estado)
+      REFERENCES estado(id_estado)
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+PRIMARY KEY(id_remesa,id_estado,fecha)
 );
