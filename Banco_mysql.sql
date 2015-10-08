db52e15947f674e72e6997e154b591ee8fe0122f
ReimerGhost <rchamale10@gmail.com>
2015-10-08 15:59:54 -0600

    Cambios en BD de banco

--- Banco_mysql.sql
+++ Banco_mysql.sql
@@ -1,32 +1,20 @@
 use bancoG8;
 
 create table cliente
-( id_cliente int primary key,
+(
+id_cliente int primary key,
 nombre varchar(20),
-apellido varchar(20),
-dpi varchar(13)
-);
-
-create table tipo_cuenta
-( id_tipo_cuenta int primary key,
-nombre varchar(25)
-);
-
-create table tipo_transaccion
-( id_tipo_trans int primary key,
-nombre varchar(25)
+apellido varchar(20)
 );
 
 create table cuenta
-( id_cuenta int primary key,
+(
+id_cuenta int primary key,
 fecha_creada Datetime,
 id_owner int not null,
 id_tipo_cuenta int not null,
 FOREIGN KEY (id_owner)
       REFERENCES cliente(id_cliente)
-      ON UPDATE CASCADE ON DELETE RESTRICT,
-FOREIGN KEY (id_tipo_cuenta)
-      REFERENCES tipo_cuenta(id_tipo_cuenta)
       ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
@@ -37,10 +25,26 @@ id_cuenta int not null,
 id_tipo_trans int not null,
 FOREIGN KEY (id_cuenta)
       REFERENCES cuenta(id_cuenta)
-      ON UPDATE CASCADE ON DELETE RESTRICT,
-FOREIGN KEY (id_tipo_trans)
-      REFERENCES tipo_transaccion(id_tipo_trans)
       ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+create table remesas_efectivo
+(
+id_remesa int,
+id_remesadora int,
+fecha Datetime,
+nombre_emisor varchar(100),
+nombre_receptor varchar(100),
+monto_Q numeric(10,2),
+monto_D numeric(10,2),
+estado varchar(3),
+primary key(id_remesa,id_remesadora)
+);
+
+create table tipo_cambio
+(
+cambioCompra numeric(10,2),
+cambioVenta numeric(10,2)
 );
 
 
