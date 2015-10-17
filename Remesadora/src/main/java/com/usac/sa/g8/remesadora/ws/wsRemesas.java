c116e66551ac1c31d64d18869e10bbf340998081
naldoc <naldoc@naldoc-laptop>
2015-10-17 02:55:25 -0600

    Tengo sueño. :O

--- Remesadora/src/main/java/com/usac/sa/g8/remesadora/ws/wsRemesas.java
+++ Remesadora/src/main/java/com/usac/sa/g8/remesadora/ws/wsRemesas.java
@@ -8,6 +8,8 @@ package com.usac.sa.g8.remesadora.ws;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.ws.WebServiceRef;
+import remesadorag8.RemesadoraG8;
 
 /**
  *
@@ -15,24 +17,81 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "wsRemesas")
 public class wsRemesas {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_9763/services/remesadorag8.wsdl")
+    private RemesadoraG8 service;
+
+    @WebMethod(operationName = "cambiarEstado")
+    public String cambiarEstado(@WebParam(name = "no_remesa") int no_remesa,
+                                @WebParam(name = "monto") int monto) {
+        
+        try { // Call Web Service Operation
+            remesadorag8.RemesadoraG8PortType port = service.getSOAP11Endpoint();
+            // TODO initialize WS operation arguments here
+            java.lang.Integer idRemesa = no_remesa;
+            // TODO process result here
+            java.util.List<remesadorag8.Remesa> result = port.selectWithKeyRemesaOperation(idRemesa);
+            if(result.isEmpty()){
+               System.out.println("REMESA NO ENCONTRADA."); 
+            }else{
+                remesadorag8.Remesa Remesa = result.get(0);
+                port.updateRemesaOperation(Remesa.getNombreEmisor(), 
+                                           Remesa.getNombreReceptor(), 
+                                           Remesa.getFechaAgregado(), 
+                                           new java.util.Date().toGMTString(), 
+                                           Remesa.getTipoRemesa(), 
+                                           Remesa.getIdCuenta().intValue(), 
+                                           Remesa.getIdPrestamo().intValue(), 
+                                           Remesa.getBancoDestino(), 
+                                           "COBRADA", 
+                                           Remesa.getMontoDolares().doubleValue(), 
+                                           Remesa.getMontoQuetzales().doubleValue(), 
+                                           Remesa.getComision().doubleValue(), 
+                                           Remesa.getIdRemesadora(), 
+                                           Remesa.getIdRemesa().intValue());
+                //System.out.println("Result = "+ Remesa.getBancoDestino());            
+            }
+            
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+
+        return "TRUE";        
+    }
 
     /**
-     * This is a sample web service operation
+     * Web service operation
      */
-    @WebMethod(operationName = "enviarRemesaACuenta")
-    public String enviarRemesaACuenta(@WebParam(name = "name") String txt) {
-        return "Hello " + "" + " !";        
-    }
-    @WebMethod(operationName = "enviarRemesaAPrestamo")
-    public String enviarRemesaAPrestamo(@WebParam(name = "name") String txt) {
-        return "Hello " + "" + " !";        
-    }
     @WebMethod(operationName = "enviarRemesaEfectivo")
-    public String enviarRemesaEfectivo(@WebParam(name = "no_remesa") String no_remesa,@WebParam(name = "no_remesadora") String no_remesadora) {
-        return "Hello " + "" + " !";        
-    }
-    @WebMethod(operationName = "cambiarEstado")
-    public String cambiarEstado(@WebParam(name = "no_remesa") int no_remesa,@WebParam(name = "monto") int monto) {
-        return "Hello " + "" + " !";        
+    public String enviarRemesaEfectivo(@WebParam(name = "emisor") String emisor, 
+                                       @WebParam(name = "receptor") String receptor, 
+                                       @WebParam(name = "montoEnDolares") double montoEnDolares, 
+                                       @WebParam(name = "bancoDestino") String banco_Destino) {
+        //TODO write your implementation code here:
+        
+        try { // Call Web Service Operation
+            remesadorag8.RemesadoraG8PortType port = service.getSOAP11Endpoint();
+            // TODO initialize WS operation arguments here
+            java.lang.String nombreEmisor = emisor;
+            java.lang.String nombreReceptor = receptor;
+            java.util.Date fechaRegistroRemesa = new java.util.Date();
+            java.lang.String fechaAgregado = fechaRegistroRemesa.toGMTString();
+            java.lang.String fechaPago = null;
+            java.lang.String tipoRemesa = "Efectivo";
+            java.lang.Integer idCuenta = null;
+            java.lang.Integer idPrestamo = null;
+            java.lang.String bancoDestino = banco_Destino;
+            java.lang.String estado = "SinCobrar";
+            java.lang.Double montoDolares = montoEnDolares;
+            java.lang.Double montoQuetzales = montoEnDolares*7.78;
+            java.lang.Double comision = montoDolares*0.01;
+            java.lang.String Remesadora = "8";
+            port.insertRemesaOperation(nombreEmisor, nombreReceptor, fechaAgregado, fechaPago, tipoRemesa, idCuenta, idPrestamo, bancoDestino, estado, montoDolares, montoQuetzales, comision, Remesadora);
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+            return "FALSE";
+        }
+
+        return "TRUE";
+
     }
 }
